8cf45168d326856fbd540da62d84d614ac95e063
gymka <gymka@archlinux.lt>
2013-03-14 12:28:39 +0200

    blogas kelias

--- oras_db.sh
+++ oras_db.sh
@@ -36,19 +36,19 @@ fi
 wget --tries=10 --retry-connrefused --wait=1 --user-agent="Firefox" --load-cookies $kelias/cookie -O $kelias/oras_db.txt $addr_now
 
 time12=$(grep -o -m 1 'updated">.*Local Time' $kelias/oras_db.txt|sed  's/.*[0-9]\{4\}, \(.*\)Local.*/\1/')
-time24=$(date -d $time12 +%H:%M>oras_db_data.txt)
-temp=$(grep -m1 'itemprop="temperature-celsius">' oras_db.txt|sed -n 's/.*celsius">\(.*\)<\/span.*/\1/p'>>oras_db_data.txt)
-sky=$(grep -m1 'itemprop="weather-phrase">' oras_db.txt|sed -n 's/.*phrase">\(.*\)<\/span.*/\1/p'>>oras_db_data.txt)
-wind=$(sed -n '/<div class="wx-data-part wx-first wx-wind">/,/km\/h<\/div>/p' oras_db.txt)
-wind_dir=$(echo $wind|sed -n 's/.*>\(.*\) at.*/\1/p'>>oras_db_data.txt)
+time24=$(date -d $time12 +%H:%M>$kelias/oras_db_data.txt)
+temp=$(grep -m1 'itemprop="temperature-celsius">' $kelias/oras_db.txt|sed -n 's/.*celsius">\(.*\)<\/span.*/\1/p'>>$kelias/oras_db_data.txt)
+sky=$(grep -m1 'itemprop="weather-phrase">' $kelias/oras_db.txt|sed -n 's/.*phrase">\(.*\)<\/span.*/\1/p'>>$kelias/oras_db_data.txt)
+wind=$(sed -n '/<div class="wx-data-part wx-first wx-wind">/,/km\/h<\/div>/p' $kelias/oras_db.txt)
+wind_dir=$(echo $wind|sed -n 's/.*>\(.*\) at.*/\1/p'>>$kelias/oras_db_data.txt)
 wind_speed=$(echo $wind|sed -n 's/.*>.*at\(.*\)km.*<\/div>/\1/p')
-wind_m=$(echo "scale=2; $wind_speed/3.6" | bc|sed 's/^\./0./'>>oras_db_data.txt)
-hum=$(grep -m1 -A1 'class="wx-label">Humidity' oras_db.txt|sed -n 's/.*wx-data">\(.*\)<\/div>/\1/p'>>oras_db_data.txt)
-visi=$(grep -m1 -A1 'class="wx-label">Visibility' oras_db.txt|sed -n 's/.*wx-data">\(.*\)<\/div>/\1/p'>>oras_db_data.txt)
-pres=$(grep -m1 -A2 'class="wx-label">Pressure' oras_db.txt|sed -n 's/ \(.*\)mb/\1mb/p'>>oras_db_data.txt)
-uv=$(grep -m1 '<span class="wx-temp" itemprop="uv-index">' oras_db.txt|sed -n 's/<.*>\(.*\)<.*>/\1/p'>>oras_db_data.txt)
-sed -i 's/\t//g;s/   //g' oras_db_data.txt
-image=$(grep -m1 http://s.imwx.com/v.*/img/wxicon/ oras_db.txt|sed -n 's/.*\/\(.*\)\.png.*/\1/p')
+wind_m=$(echo "scale=2; $wind_speed/3.6" | bc|sed 's/^\./0./'>>$kelias/oras_db_data.txt)
+hum=$(grep -m1 -A1 'class="wx-label">Humidity' $kelias/oras_db.txt|sed -n 's/.*wx-data">\(.*\)<\/div>/\1/p'>>$kelias/oras_db_data.txt)
+visi=$(grep -m1 -A1 'class="wx-label">Visibility' $kelias/oras_db.txt|sed -n 's/.*wx-data">\(.*\)<\/div>/\1/p'>>$kelias/oras_db_data.txt)
+pres=$(grep -m1 -A2 'class="wx-label">Pressure' $kelias/oras_db.txt|sed -n 's/ \(.*\)mb/\1mb/p'>>$kelias/oras_db_data.txt)
+uv=$(grep -m1 '<span class="wx-temp" itemprop="uv-index">' $kelias/oras_db.txt|sed -n 's/<.*>\(.*\)<.*>/\1/p'>>$kelias/oras_db_data.txt)
+sed -i 's/\t//g;s/   //g' $kelias/oras_db_data.txt
+image=$(grep -m1 http://s.imwx.com/v.*/img/wxicon/ $kelias/oras_db.txt|sed -n 's/.*\/\(.*\)\.png.*/\1/p')
 cp $kelias/piktogramos/$image.png $kelias/now.png
 $kelias/isverst_ora.sh
 
