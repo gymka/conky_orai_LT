85af96f6f882e8be77ed49348e4c3e4c274b50e2
gymka <gymka@archlinux.lt>
2014-03-09 11:02:31 +0200

    v1.0

--- main.py
+++ main.py
@@ -3,25 +3,26 @@
 import re
 import urllib.request
 import os
+import random
 
 #======Duomenų parsiuntimas=======
-user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:27.0) Gecko/20100101 Firefox/27.0'
-headers = { 'User-Agent' : user_agent }
+headers = { "User-Agent" : "Mozilla/5.0 (X11; Linux x86_64; rv:27.0) Gecko/20100101 Firefox/27.0", "Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "Accept-Language" : "lt,en;q=0.7,ru;q=0.3", "Accept-Encoding" : "gzip, deflate", "DNT" : "1", "Connection" : "keep-alive" } #nesiunčiant header'ių po kažkiek laiko nebeleidžia prisijungt:)
+
 url="http://www.yr.no/place/Lithuania/Kaunas/Kaunas/long.html"
 url2="http://www.yr.no/place/Lithuania/Kaunas/Kaunas/"
 klaida=False
+orai_html_long=""
 try:
 	orai_html_long = urllib.request.urlopen(urllib.request.Request(url, headers={'User-Agent': user_agent})).read().decode("utf-8")
 except:
 	klaida=True
 try:
 	orai_html_short = urllib.request.urlopen(urllib.request.Request(url2, headers={'User-Agent': user_agent})).read().decode("utf-8")
+	orai_short=re.split("<td> 11:00–17:00|14:00–20:00</td>",orai_html_short)
+	orai_short=orai_short[1].split("</tr>")
 except:
 	klaida=True
 klaidos_rez=("??","??","??","??","??","??","??","??","??")
-orai_short=re.split("<td> 11:00–17:00|14:00–20:00</td>",orai_html_short)
-orai_short=orai_short[1].split("</tr>")
-#print(orai_short[0])
 
 #=======Duomenų apdorojimas=========
 class orai():
@@ -63,14 +64,14 @@ class orai():
 	def vejas_diena(source):
 		#grąžina masyvą su 3 elementais
 		if klaida is not True:
-			return re.findall("<td class=\"txt-left\" title=\"(.*), (.*m/s) (.*)For the period: (11:00|14:00)\">",source,re.IGNORECASE)
+			return re.findall("<td class=\"txt-left\" title=\"(.*), (.*m/s) from (.*)\.  For the period: (11:00|14:00)\">",source,re.IGNORECASE)
 		else:
 			return klaidos_rez
 			
 	def vejas_naktis(source):
 		#grąžina masyvą su 3 elementais
 		if klaida is not True:
-			return re.findall("<td class=\"txt-left\" title=\"(.*), (.*m/s) (.*)For the period: (23:00|02:00)\">",source,re.IGNORECASE)
+			return re.findall("<td class=\"txt-left\" title=\"(.*), (.*m/s) from (.*)\.  For the period: (23:00|02:00)\">",source,re.IGNORECASE)
 		else:
 			return klaidos_rez
 
@@ -96,26 +97,34 @@ class orai_day():
 			return re.findall("class=\"wind\" alt=\"(.*), (.*m/s) from (.*)\" ",orai_short[0],re.IGNORECASE)[0]
 		else:
 			return klaidos_rez			
+def isverst_veja(vejas):
+	veju_array={"south":("pietų","\u2191"),"north":("šiaurės","\u2193"), "east":("rytų","\u2190"), "west":("vakarų","\u2192"), "northeast":("šiaurės rytų","\u2199"), "northwest":("šiaurės vakarų","\u2198"), "southeast":("pietryčių","\u2196"), "southwest":("pietvakarių","\u2197"), "north-northeast":("šiaurės šiaurės rytų","\u2199"), "south-southwest":("pietų pietryčių","\u2197"), "west-southwest":("vakarų pietvakarių","\u2197"), "south-southeast":("pietų pietryčių","\u2196"), "east-southeast":("rytų pietryčių","\u2196"), "east-northeast":("rytų šiaurės rytų","\u2199"), "north-northwest":("rytų šiaurės vakarų","\u2198"), "west-northwest":("vakarų šiaurės vakarų","\u2198")}
+	return veju_array[vejas]
+isverst_diena={"Monday":"Pirmadienis","Tuesday":"Antradienis","Wednesday":"Trečiadienis","Thursday":"Ketvirtadienis","Friday":"Penktadienis","Saturday":"Šeštadienis","Sunday":"Sekmadienis"}
+
 
 
 #surašom duomenis į failą
-failas=open("orai.txt","w")
-failas.write("="*10+"Prognozė"+"="*10+"\n")
-for i in range(0,9): #TODO jei pasikeis kodas ir ras mažiau elementų, bus bėda.
-	failas.write(orai.dienos(orai_html_long)[i]+"\n")
-	os.system('ln -s -f ${PWD}/paveiksliukai/'+orai.dangus_diena(orai_html_long)[i][1]+' ./'+str(i)+'.png')
-	os.system('ln -s -f ${PWD}/paveiksliukai/'+orai.dangus_nakti(orai_html_long)[i][1]+' ./'+str(i)+'N.png')
-	failas.write(orai.temp_nakti(orai_html_long)[i][1]+"\n")
-	failas.write(orai.temp_diena(orai_html_long)[i][1]+"\n")
+if klaida is not True:
+	failas=open("orai.txt","w")
+	failas.write("="*10+"Prognozė"+"="*10+"\n")
+	for i in range(0,9): #TODO jei pasikeis kodas ir ras mažiau elementų, bus bėda.
+		failas.write(isverst_diena[orai.dienos(orai_html_long)[i]]+"\n")
+		vejasx=orai.vejas_diena(orai_html_long)[i][2]
+		failas.write(isverst_veja(vejasx)[0]+"\n")
+		failas.write(isverst_veja(vejasx)[1]+"\n")
+		os.system('ln -s -f ${PWD}/paveiksliukai/'+orai.dangus_diena(orai_html_long)[i][1]+' ./'+str(i)+'.png')
+		os.system('ln -s -f ${PWD}/paveiksliukai/'+orai.dangus_nakti(orai_html_long)[i][1]+' ./'+str(i)+'N.png')
+		failas.write(orai.temp_nakti(orai_html_long)[i][1]+"\n")
+		failas.write(orai.temp_diena(orai_html_long)[i][1]+"\n")
+		failas.write("-"*20+"\n")
+	failas.write("="*10+"Dienos prognozė"+"="*10+"\n")
+	os.system('ln -s -f ${PWD}/paveiksliukai/'+orai_day.dangus()+' ./dabar.png')
+	failas.write(orai_day.temp()+"\n")
+	failas.write(orai_day.krituliai()+"\n")
+	failas.write(orai_day.vejas()[1]+"\n")
+	failas.write(isverst_veja(orai_day.vejas()[2])[0]+"\n")
+	failas.write(isverst_veja(orai_day.vejas()[2])[1]+"\n")
 	failas.write("-"*20+"\n")
-
-failas.write("="*10+"Dienos prognozė"+"="*10+"\n")
-os.system('ln -s -f ${PWD}/paveiksliukai/'+orai_day.dangus()+' ./now.png')
-failas.write(orai_day.temp()+"\n")
-failas.write(orai_day.krituliai()+"\n")
-failas.write(orai_day.vejas()[0]+"\n")
-failas.write(orai_day.vejas()[1]+"\n")
-failas.write(orai_day.vejas()[2]+"\n")
-failas.write("-"*20+"\n")
-failas.close()
+	failas.close()
 
